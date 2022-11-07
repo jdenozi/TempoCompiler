e2e97347ae4ab64c9cd3eda64a802922fee61f46
denozi <j-denozi@outlook.com>
2022-11-07 16:04:36 +0100

    Add GTest

---
+++ include/Lexer/Lexer.h
@@ -0,0 +1,19 @@
+#ifndef TOTOCOMPILER_LEXER_H
+#define TOTOCOMPILER_LEXER_H
+
+#include "iostream"
+
+namespace TotoCompiler::Lexer::Lexer {
+    class Lexer {
+    public:
+
+        explicit Lexer(std::streambuf *stream): m_stream(stream){
+        };
+
+
+    private:
+        std::streambuf *m_stream;
+
+    };
+}
+#endif //TOTOCOMPILER_LEXER_H
