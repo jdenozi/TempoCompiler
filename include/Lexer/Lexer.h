3677d7edea4c91f000878eba771d199dc4e23236
juliend <j-denozi@outlook.com>
2022-11-10 17:02:53 +0100

    Add Token type to the lexer

--- include/Lexer/Lexer.h
+++ include/Lexer/Lexer.h
@@ -5,6 +5,10 @@
 #include "iostream"
 
 namespace TotoCompiler::Lexer {
+    /**
+     *  it will iterate character by character to do two things: decide where each token starts/stops and what type of token it is.
+     *  If the lexer is unable to do this, then it will report an error for an invalid token.
+     */
     class Lexer {
     public:
         explicit Lexer(std::ifstream &&stream): m_stream(std::move(stream)){
--- include/Lexer/Token.h
+++ include/Lexer/Token.h
@@ -2,5 +2,40 @@
 #define TOTOCOMPILER_TOKEN_H
 
 
+namespace TotoCompiler::Lexer{
+    enum class TokenType{
+        Number,
+        Identifier,
+        LeftParenthesis,
+        RightParenthesis,
+        LeftBracket,
+        RightBracket,
+        LeftBrace,
+        RightBrace,
+        LessThan,
+        GreaterThan,
+        Equal,
+        Plus,
+        Minus,
+        Asterisk,
+        Slash,
+        Hash,
+        Dot,
+        Comma,
+        Colon,
+        Semicolon,
+        SingleQuote,
+        DoubleQuote,
+        Comment,
+        Pipe,
+        End,
+        Unexpected,
+    };
+
+    template<typename T>
+    class Token{
+
+    };
+}
 
 #endif //TOTOCOMPILER_TOKEN_H
